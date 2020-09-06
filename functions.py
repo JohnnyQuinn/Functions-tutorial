0cc1e1bcdcf6638db3141676eb7996865cfae3aa
johnnyquinn <jftq97@gmail.com>
2020-09-06 15:40:18 -0400

    Completed
    Completed assignment

---
+++ functions.py
@@ -0,0 +1,23 @@
+def area(width, height):
+    result = width * height
+    return result
+
+result = area(5, 6)
+print(result)
+
+area(4,7)
+area(3,8)
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+print(subtract(10,5))
+print(subtract(4,1))
+
+def add(num1, num2):
+    result = num1 + num2
+    return result
+
+print(add(1,2))
+print(add(4,7))
